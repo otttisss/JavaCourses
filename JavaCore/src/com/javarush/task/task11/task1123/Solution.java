efe8ab8cae9edf7a656b825ecec18843fed1ff2e
otttisss <otttisss@yandex.ru>
2023-02-09 14:42:56 +0300

    level 11 completed

---
+++ JavaCore/src/com/javarush/task/task11/task1123/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task11.task1123;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair result = getMinimumMaximumPair(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair getMinimumMaximumPair(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair(null, null);
+        }
+
+        // напишите тут ваш код
+        int x = inputArray[0];
+        int y = inputArray[0];
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] < x)
+                x = inputArray[i];
+            if (inputArray[i] > y)
+                y = inputArray[i];
+        }
+
+        return new Pair(x, y);
+    }
+
+    public static class Pair {
+        public Integer x;
+        public Integer y;
+
+        public Pair(Integer x, Integer y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
