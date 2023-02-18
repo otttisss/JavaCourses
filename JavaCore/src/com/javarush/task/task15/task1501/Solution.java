387af1d7fdc73db6e1854992adcca4c9b57d3411
otttisss <otttisss@yandex.ru>
2023-02-18 14:40:02 +0300

    15 lvl started

---
+++ JavaCore/src/com/javarush/task/task15/task1501/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task15.task1501;
+
+/* 
+ООП - Расставить интерфейсы
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Movable {
+        boolean isMovable();
+    }
+
+    public interface Sellable {
+        Object getAllowedAction(String name);
+    }
+
+    public interface Discountable {
+        Object getAllowedAction();
+    }
+
+    public static class Clothes implements Movable, Sellable, Discountable {
+        public boolean isMovable(){
+            return false;
+        }
+        public Object getAllowedAction(String name){
+            return null;
+        }
+        public Object getAllowedAction(){
+            return null;
+        }
+    }
+}
