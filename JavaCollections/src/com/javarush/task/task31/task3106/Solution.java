82e529fab9d06f6692f4fc22cc7a3e094c5565dc
otttisss <otttisss@yandex.ru>
2023-06-08 15:42:56 +0300

    second part completed

---
+++ JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task31.task3106;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/* 
+Разархивируем файл
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String resultFileName = args[0];
+        int fileParts = args.length - 1;
+        String[] fileNamePart = new String[fileParts];
+        
+    }
+}
--- JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
+++ JavaCollections/src/com/javarush/task/task31/task3113/Solution.java
@@ -1,10 +1,12 @@
 package com.javarush.task.task31.task3113;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -13,7 +15,36 @@ import java.util.concurrent.atomic.AtomicLong;
 */
 
 public class Solution {
-
+    static long totalDirs = 0;
+    static long totalFiles = 0;
+    static long totalSize = 0;
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        reader.close();
+
+        Path path = Paths.get(str);
+        if (!Files.isDirectory(path)) {
+            System.out.println(path.toAbsolutePath().toString() + " - не папка");
+        }
+        Files.walkFileTree(path, new Visitor());
+        System.out.println("Всего папок - " + (totalDirs-1));
+        System.out.println("Всего файлов - " + totalFiles);
+        System.out.println("Общий размер - " + totalSize);
+    }
+    public static class Visitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            totalDirs += 1;
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            totalFiles += 1;
+            totalSize = totalSize + attrs.size();
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
+
